27840b1d749ce0c182778f8fce9a5af66af99f7f
hyunwoo Lee <hw.ralf79@gmail.com>
2017-09-20 22:28:51 +0900

    init commit

---
+++ src/main/java/com/easyspringboot/EasySpringbootApplication.java
@@ -0,0 +1,12 @@
+package com.easyspringboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EasySpringbootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EasySpringbootApplication.class, args);
+	}
+}
